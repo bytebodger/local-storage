74b3d0f90e171aa120875129e9a4b6b1c22209e7
Adam Nathaniel Davis <adam.davis@hashverify.com>
2020-03-19 22:16:34 -0400

    Update local.js

--- local.js
+++ local.js
@@ -1,42 +1,48 @@
-import is from './is.js';
+import is from './is';
 
-class localStorageWrapper {
+class local {
    /*
       a wrapper for localStorage() that will store every key value in a serialized JSON string
       this allows localStorage to hold string, booleans, numbers, nulls, objects, and arrays
     */
-   getItem(itemName) {
-      if (!is.aPopulatedString(itemName)) { return null; }
+   getItem = (itemName = '') => {
+      if (!is.aPopulatedString(itemName))
+         return null;
       const valueToBeDeserialized = localStorage.getItem(itemName);
-      if (!valueToBeDeserialized) { return null; }
+      if (!valueToBeDeserialized)
+         return null;
       const deserializedValue = JSON.parse(valueToBeDeserialized);
-      if (deserializedValue.hasOwnProperty('value')) { return deserializedValue.value; }
+      if (deserializedValue.hasOwnProperty('value'))
+         return deserializedValue.value;
       return null;
-   }
+   };
    
-   removeItem(itemName) {
-      if (!is.aPopulatedString(itemName)) { return false; }
+   removeItem = (itemName = '') => {
+      if (!is.aPopulatedString(itemName))
+         return false;
       localStorage.removeItem(itemName);
       return true;
-   }
+   };
    
-   setDefault(itemName, defaultValue) {
-      if (!is.aPopulatedString(itemName) || defaultValue === undefined) { return null; }
+   setDefault = (itemName = '', defaultValue = '') => {
+      if (!is.aPopulatedString(itemName) || defaultValue === undefined)
+         return null;
       const currentValue = this.getItem(itemName);
-      if (currentValue !== null && currentValue !== undefined) { return this.getItem(itemName); }
+      if (!is.nullOrUndefined(currentValue) && (currentValue !== '' || currentValue === defaultValue))
+         return this.getItem(itemName);
       this.setItem(itemName, defaultValue);
       return defaultValue;
-   }
+   };
    
-   setItem(itemName, itemValue) {
-      if (is.undefined(itemValue)) { return false; }
-      if (!is.aPopulatedString(itemName)) { throw new TypeError(itemName.toString()); }
+   setItem = (itemName, itemValue) => {
+      if (is.undefined(itemValue) || !is.aPopulatedString(itemName))
+         return false;
       const valueToBeSerialized = {value : itemValue};
       const serializedValue = JSON.stringify(valueToBeSerialized);
       localStorage.setItem(itemName, serializedValue);
       return true;
-   }
+   };
 }
 
-const local = new localStorageWrapper();
-export default local;
+const db = new local();
+export default db;
